e38ef2b53976a60c4308608898a60a734d8996fa
ShirakinRirishiyo <143818618+ShirakinRirishiyo@users.noreply.github.com>
2025-06-04 08:46:53 +0000

    changes in signals

--- src/signals_handler.c
+++ src/signals_handler.c
@@ -1,7 +1,27 @@
 #include "../include/minishell.h"
 
 
-void csignal(int sig)
-{
 
+void handle_sigint(int sig)
+{
+    (void)sig;
+
+    //clear current line
+    write(STDOUT_FILENO, "\n", 1);
+    //erase the current line
+    rl_replace_line("", 0);
+    //move the cursor to the beginning of the line
+    rl_on_new_line();
+    //redisplay prompt
+    rl_redisplay();
+
+}
+
+void init_signals(void) {
+    
+    sa.sa_handler = handle_sigint;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+
+    sigaction(SIGINT, &sa, NULL);
 }
